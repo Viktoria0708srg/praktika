2dd343d351f4e18b815caf4850f39df985460bee
Viktoria0708srg <147665659+Viktoria0708srg@users.noreply.github.com>
2025-12-21 14:34:18 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import socket
+
+def check_port(ip, port):
+    sock = socket.socket()
+    try:
+        sock.connect((ip, port))
+        sock.close()
+        return True
+    except:
+        return False
+
+ports_to_check = [21, 22, 23, 25, 53, 80, 110, 143, 443, 445, 3389, 5900]
+
+ip = input("Введите IP-адрес: ")
+
+print(f"\nСканирование {ip}...\n")
+
+for port in ports_to_check:
+    if check_port(ip, port):
+        print(f"Порт {port}: ОТКРЫТ")
+    else:
+        print(f"Порт {port}: закрыт")
+print("Готово!")
+
+
+     
+    
+
